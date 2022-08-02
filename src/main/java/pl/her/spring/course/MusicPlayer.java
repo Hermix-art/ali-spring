97ee9934686ecc719ef01546a4f19320b9e534a8
herman_kulik <herman_kulik@epam.com>
2022-08-02 09:21:24 +0200

    autowired on field

--- src/main/java/pl/her/spring/course/MusicPlayer.java
+++ src/main/java/pl/her/spring/course/MusicPlayer.java
@@ -5,23 +5,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-  private Music music;
 
-  public Music getMusic() {
-    return music;
-  }
+    @Autowired
+    private Music music;
 
-  @Autowired
-  public void setMusic(Music music) {
-    this.music = music;
-  }
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
 
-  public MusicPlayer(Music music) {
-    this.music = music;
-  }
-
-  public void playMusic() {
-    System.out.printf("Playing the %s music", music.getSong());
-  }
+    public void playMusic() {
+        System.out.printf("Playing the %s music", music.getSong());
+    }
 
 }
