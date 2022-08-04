5fe4aceeb94b5f0b847654405eee8a6efd94fb43
herman_kulik <herman_kulik@epam.com>
2022-08-04 16:28:38 +0200

    predestroy and postconstruct

--- src/main/java/pl/her/spring/course/ClassicalMusic.java
+++ src/main/java/pl/her/spring/course/ClassicalMusic.java
@@ -1,11 +1,23 @@
 package pl.her.spring.course;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("someClassicalMusic")
-@Scope("prototype")
+@Scope("singleton")
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void printInTheBeginning(){
+        System.out.println("hello guys, let's start!");
+    }
+
+    @PreDestroy
+    void youWannaDestroyMe(){
+        System.out.println("It's destruction time, babe!");
+    }
 
     @Override
     public String getSong() {
